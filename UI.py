0ebcc2ea6c61b5880a0f144ae6ffedfe8c8685a3
delvinlow <delvinlowzy@gmail.com>
2016-09-18 14:53:16 +0800

    Change to OOP and add app icon

--- UI.py
+++ UI.py
@@ -1,12 +1,20 @@
 import sys
 from PyQt4 import QtGui
 
+
+class Window(QtGui.QMainWindow):
+	def __init__(self):
+		super(Window, self).__init__()
+		self.setGeometry(50, 50, 500, 300)
+		self.setWindowTitle("CS2108 Image Search")
+		self.setWindowIcon(QtGui.QIcon("assets/ic_image_search.png"))
+
+		self.show()
+
+
+
 app = QtGui.QApplication(sys.argv)
 
-window = QtGui.QWidget()
-
-window.show()
-
-window.setWindowTitle("CS2108 A1")
+GUI = Window()
 
 sys.exit(app.exec_())
