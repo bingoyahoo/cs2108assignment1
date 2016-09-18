3209c40b2c55e1a269abdedf205c2e3e8a6f0b49
delvinlow <delvinlowzy@gmail.com>
2016-09-18 15:20:05 +0800

    Added Quit, Choose image and Search buttons

--- UI.py
+++ UI.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
 class Window(QtGui.QMainWindow):
@@ -9,12 +9,41 @@ class Window(QtGui.QMainWindow):
 		self.setWindowTitle("CS2108 Image Search")
 		self.setWindowIcon(QtGui.QIcon("assets/ic_image_search.png"))
 
+		self.home()
+
+	def home(self):
+		"""Specific to page"""
+		btn = QtGui.QPushButton("Quit", self)
+		btn.clicked.connect(self.close_application)
+		btn.resize(btn.minimumSizeHint())
+
+		btn_picker = QtGui.QPushButton("Choose an image", self)
+		btn_picker.clicked.connect(self.choose_image)
+		btn_picker.resize(btn.minimumSizeHint())
+		btn_picker.move(100, 100)
+
+		btn_picker = QtGui.QPushButton("Search", self)
+		btn_picker.clicked.connect(self.search_image)
+		btn_picker.resize(btn.minimumSizeHint())
+		btn_picker.move(200, 100)
+
 		self.show()
 
+	def close_application(self):
+		print "Closing"
+		sys.exit()
+
+	def choose_image(self):
+		pass
+
+	def search_image(self):
+		pass
 
 
-app = QtGui.QApplication(sys.argv)
 
-GUI = Window()
+def main():
+	app = QtGui.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
 
-sys.exit(app.exec_())
+main()
